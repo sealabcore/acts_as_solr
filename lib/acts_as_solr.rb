5b63df9fd5e09658b6031bb77515656651a10969
Kyle Maxwell <kyle@kylemaxwell.com>
2009-08-25 11:32:03 -0700

    env compatibility

--- lib/acts_as_solr.rb
+++ lib/acts_as_solr.rb
@@ -36,13 +36,16 @@ module ActsAsSolr
   class Post    
     def self.execute(request)
       begin
-        if File.exists?(RAILS_ROOT+'/config/solr.yml')
-          config = YAML::load_file(RAILS_ROOT+'/config/solr.yml')
-          url = config[RAILS_ENV]['url']
-          # for backwards compatibility
-          url ||= "http://#{config[RAILS_ENV]['host']}:#{config[RAILS_ENV]['port']}/#{config[RAILS_ENV]['servlet_path']}"
-        else
-          url = 'http://localhost:8982/solr'
+        # if File.exists?(RAILS_ROOT+'/config/solr.yml')
+        #   config = YAML::load_file(RAILS_ROOT+'/config/solr.yml')
+        #   url = config[RAILS_ENV]['url']
+        #   # for backwards compatibility
+        #   url ||= "http://#{config[RAILS_ENV]['host']}:#{config[RAILS_ENV]['port']}/#{config[RAILS_ENV]['servlet_path']}"
+        # else
+        #   url = 'http://localhost:8982/solr'
+        # end
+        unless url = ENV["WEBSOLR_URL"]
+          raise "WEBSOLR_URL was not defined.  Have you run websolr configure?"
         end
         connection = Solr::Connection.new(url)
         return connection.send(request)
