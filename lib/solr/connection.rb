55f793ecc81fd77fe516bd70bd20c62abed9ada4
Kyle Maxwell <kyle@kylemaxwell.com>
2009-10-12 03:48:39 -0700

    better regex

--- lib/solr/connection.rb
+++ lib/solr/connection.rb
@@ -17,7 +17,8 @@ require 'net/http'
 class Solr::Connection
   attr_reader :url, :autocommit, :connection
   
-  ILLEGAL_XML_CHARS = /[\x00-\x1F]/
+  ILLEGAL_XML_CHARS =  /\x00|\x01|\x02|\x03|\x04|\x05|\x06|\x07|\x08|\x0B|\x0C|\x0E|\x0F|\x10|\x11|\x12|\x13|\x14|\x15|\x16|\x17|\x18|\x19|\x1A|\x1B|\x1C|\x1D|\x1E|\x1F/ 
+  
 
   # create a connection to a solr instance using the url for the solr
   # application context:
--- websolr-rails.gemspec
+++ websolr-rails.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{onemorecloud-websolr-rails}
-  s.version = "1.4.4"
+  s.version = "1.4.6"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Kyle Maxwell"]
