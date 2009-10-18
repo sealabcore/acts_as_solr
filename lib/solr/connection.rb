cfe13fde6f123db9a6603ed0c9257f8792d1d992
Kyle Maxwell <kyle@kylemaxwell.com>
2009-10-17 18:39:31 -0700

    backport fix for bug where invalid chars would sneak into the xml

--- lib/solr/connection.rb
+++ lib/solr/connection.rb
@@ -17,6 +17,8 @@ require 'net/http'
 class Solr::Connection
   attr_reader :url, :autocommit, :connection
 
+  ILLEGAL_XML_CHARS =  /\x00|\x01|\x02|\x03|\x04|\x05|\x06|\x07|\x08|\x0B|\x0C|\x0E|\x0F|\x10|\x11|\x12|\x13|\x14|\x15|\x16|\x17|\x18|\x19|\x1A|\x1B|\x1C|\x1D|\x1E|\x1F/ 
+
   # create a connection to a solr instance using the url for the solr
   # application context:
   #
@@ -156,7 +158,7 @@ class Solr::Connection
   # to some requests: add(), query(), commit(), delete() or send()
   def post(request)
     response = @connection.post(@url.path + "/" + request.handler,
-                                request.to_s,
+                                request.to_s.gsub(ILLEGAL_XML_CHARS, ''),
                                 { "Content-Type" => request.content_type })
   
     case response
@@ -177,3 +179,6 @@ private
   end
   
 end
+
+
+
