625ffd32d886b78b7751b91cefdbc83ab47fd01c
Kyle Maxwell <kyle@kylemaxwell.com>
2010-03-02 21:41:39 -0800

    Version bump to 1.2.0

--- config/solr_environment.rb
+++ config/solr_environment.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV']  = (ENV['RAILS_ENV'] || 'development').dup
 # RAILS_ROOT isn't defined yet, so figure it out.
-rails_root_dir = "#{File.dirname(File.expand_path(__FILE__))}/../../../../"
+
+rails_root_dir = "#{File.expand_path('.')}"
 SOLR_PATH = "#{File.dirname(File.expand_path(__FILE__))}/../solr" unless defined? SOLR_PATH
 
 SOLR_LOGS_PATH = "#{rails_root_dir}/log" unless defined? SOLR_LOGS_PATH
@@ -10,10 +11,20 @@ SOLR_DATA_PATH = "#{rails_root_dir}/solr/#{ENV['RAILS_ENV']}" unless defined? SO
 unless defined? SOLR_PORT
   config = YAML::load_file(rails_root_dir+'/config/solr.yml')
   
-  SOLR_PORT = ENV['PORT'] || URI.parse(config[ENV['RAILS_ENV']]['url']).port
+  begin
+    SOLR_PORT = ENV['PORT'] || URI.parse(config[ENV['RAILS_ENV']]['url']).port
+  rescue
+    STDERR.puts "Couldn't set Solr port"
+    SOLR_PORT = 8982
+  end
 end
 
-SOLR_JVM_OPTIONS = config[ENV['RAILS_ENV']]['jvm_options'] unless defined? SOLR_JVM_OPTIONS
+begin
+  SOLR_JVM_OPTIONS = config[ENV['RAILS_ENV']]['jvm_options'] unless defined? SOLR_JVM_OPTIONS
+rescue 
+  STDERR.puts "Couldn't set JVM Options"
+  SOLR_JVM_OPTIONS = ""
+end
 
 if ENV['RAILS_ENV'] == 'test'
   DB = (ENV['DB'] ? ENV['DB'] : 'mysql') unless defined?(DB)
